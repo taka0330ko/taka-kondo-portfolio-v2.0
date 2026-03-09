f31a6d22d945aadf7f3c27f8ab7cda4b744419a9
Taka0330-jp <gtakanari0330@gmail.com>
2026-03-09 00:20:36 -0700

    feat:projects data file

---
+++ src/data/projects.ts
@@ -0,0 +1,35 @@
+export const projects = [
+    {
+    slug: "tastebuds",
+    cover: "src/assets/images/projects/tastebuds/tastebuds-thumbnail.jpg",
+    title: "Tastebuds - Food Blog Site",
+    description:
+      "Food blog project documenting my JavaScript journey from a Vanilla JS MVP to React and Next.js.",
+      tag:"Web dev",
+    gridClass: "col-start-1 col-end-7 row-start-1 row-end-2",
+  },
+  {
+    slug: "billow",
+    cover: "src/assets/images/projects/billow/billow-thumbnail.jpg",
+    title: "Billow - AI chat base Dashboard",
+    description: "AI chat–based dashboard built with Next.js, focusing on conversational UI and chat interaction design.",
+    tag:"Web dev",
+    gridClass: "col-start-7 col-end-13 row-start-1 row-end-2",
+  },
+  {
+    slug: "nexfit",
+    cover: "src/assets/images/projects/nexfit/nexfit-thumbnail.jpg",
+    title: "Nexfit",
+    description: "Fitness branding project.",
+    tag:"UI/UX",
+    gridClass: "col-start-1 col-end-7 row-start-2 row-end-3",
+  },
+  {
+    slug: "stone-and-honey",
+    cover: "src/assets/images/projects/stone-and-honey/stone-honey-thumbnail.jpg",
+    title: "Stone & Honey",
+    description: "Butter nuts project",
+    tag:"UI/UX",
+    gridClass: "col-start-7 col-end-13 row-start-2 row-end-3",
+  },
+]
