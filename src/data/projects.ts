8b3cea3d55ed6c83417cc1f8a1c2f59591c36623
Taka0330-jp <gtakanari0330@gmail.com>
2026-03-11 01:47:12 -0700

    fix:header path and Thumbnail path

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,22 @@
-export const projects = [
+import type { ImageMetadata } from "astro";
+import BillowThumbnail from "../assets/images/projects/billow/billow-thumbnail.jpg";
+import NexfitThumbnail from "../assets/images/projects/nexfit/nexfit-thumbnail.jpg";
+import StoneAndHoneyThumbnail from "../assets/images/projects/stone-and-honey/stone-honey-thumbnail.jpg";
+import TastebudsThumbnail from "../assets/images/projects/tastebuds/tastebuds-thumbnail.jpg";
+
+type Project = {
+  href: string;
+  cover: ImageMetadata;
+  title: string;
+  description: string;
+  tag: string;
+  gridClass: string;
+};
+
+export const projects: Project[] = [
     {
     href: "/tastebuds",
-    cover: "src/assets/images/projects/tastebuds/tastebuds-thumbnail.jpg",
+    cover: TastebudsThumbnail,
     title: "Tastebuds - Food Blog Site",
     description:
       "Food blog project documenting my JavaScript journey from a Vanilla JS MVP to React and Next.js.",
@@ -10,7 +25,7 @@ export const projects = [
   },
   {
     href: "/billow",
-    cover: "src/assets/images/projects/billow/billow-thumbnail.jpg",
+    cover: BillowThumbnail,
     title: "Billow ",
     description: "AI chat–based dashboard built with Next.js, focusing on conversational UI and chat interaction design.",
     tag:"Web dev",
@@ -18,7 +33,7 @@ export const projects = [
   },
   {
     href: "/nexfit",
-    cover: "src/assets/images/projects/nexfit/nexfit-thumbnail.jpg",
+    cover: NexfitThumbnail,
     title: "Nexfit",
     description: "Fitness branding project.",
     tag:"UI/UX",
@@ -26,7 +41,7 @@ export const projects = [
   },
   {
     href: "/stone-and-honey",
-    cover: "src/assets/images/projects/stone-and-honey/stone-honey-thumbnail.jpg",
+    cover: StoneAndHoneyThumbnail,
     title: "Stone & Honey",
     description: "Butter nuts project",
     tag:"UI/UX",
