8e3bf90a3fbe770572a8db7e1b3cc1432568a2ff
Taka0330-jp <gtakanari0330@gmail.com>
2026-03-14 12:11:09 -0700

    chore:organized code

---
+++ src/data/icons.ts
@@ -0,0 +1,19 @@
+import CSS from "../assets/images/icons/stacks/css-3.svg";
+import HTML from "../assets/images/icons/stacks/html-1.svg";
+import JS from "../assets/images/icons/stacks/js-logo.svg";
+import NextJS from "../assets/images/icons/stacks/nextjs-svgrepo-com.svg";
+import ReactLogo from "../assets/images/icons/stacks/reactjs-icon.svg";
+import SASS from "../assets/images/icons/stacks/sass-logo.svg";
+import Supabase from "../assets/images/icons/stacks/supabase-icon.svg";
+import Tailwind from "../assets/images/icons/stacks/tailwindcss-mark.svg";
+
+export {
+    CSS,
+    HTML,
+    JS,
+    NextJS,
+    ReactLogo,
+    SASS,
+    Supabase,
+    Tailwind
+}
